f283a0b061cb3a8aaf5348ae138db1816e32329d
Karl Heitmann <khf1988@gmail.com>
2015-10-26 19:20:53 -0300

    agregada solucion de Karl Heitmann mas algunos archivos de tests como sugerencias

---
+++ curl-tests/resta.sh
@@ -0,0 +1,2 @@
+#curl -d "query[op]=-", "query[ops]=[5,3]" localhost:3000
+curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ' {"query":{"op":"-","ops":[5, 3]}}'  localhost:8888
